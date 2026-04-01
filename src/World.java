425dd11e1c30af3b246872396c9437c29baa3922
Park, Juwon <won030702@gmail.com>
2026-04-01 11:02:28 +0900

    five

--- src/World.java
+++ src/World.java
@@ -1,13 +1,8 @@
-ublic class World { 
-
- 
-
-  public static void run() { 
-
-    System.out.println("202214129"); 
-
-  } 
-
- 
-
+public class World { 
+    public static void run() { 
+        System.out.println("202214129"); 
+    } 
+    public static void run2() { 
+        System.out.println("hello world"); 
+    } 
 } 
